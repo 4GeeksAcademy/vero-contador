a3bb334a2134d36b554b7a1c60ace5df6104985e
Vero519 <voviedo6@gmail.com>
2024-07-08 15:40:29 +0000

    cronometro

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 
-const Card =({number1, number2, number3, number4, number5, number6, number7 })=> {
-    return(
-<div className="d-flex">
-    <p>{number7}</p>
-    <p>{number6}</p>
-    <p>{number5}</p>
-    <p>{number4}</p>
-    <p>{number3}</p>
-    <p>{number2}</p>
-    <p>{number1}</p>
-</div>
+const Card = ({ number1, number2, number3, number4, number5, number6, number7 }) => {
+    return (
+        <div className="d-flex justify-content-center mt-4">
+            <p className="border border-1 border-primary fs-1" 
+            style={{width:"100px",height:"80px", marginRight:"10px"}}>
+                {number7}
+            </p>
+            <p className="border border-1 border-primary fs-1" 
+            style={{width:"100px",height:"80px", marginRight:"10px"}}>
+                {number6}
+            </p>
+            <p className="border border-1 border-primary fs-1" 
+            style={{width:"100px",height:"80px", marginRight:"10px"}}>
+                {number5}
+            </p>
+            <p className="border border-1 border-primary fs-1" 
+            style={{width:"100px",height:"80px", marginRight:"10px"}}>
+                {number4}
+            </p>
+            <p className="border border-1 border-primary fs-1" 
+            style={{width:"100px",height:"80px", marginRight:"10px"}}>
+                {number3}
+            </p>
+            <p className="border border-1 border-primary fs-1" 
+            style={{width:"100px",height:"80px", marginRight:"10px"}}>
+                {number2}
+            </p>
+            <p className="border border-1 border-primary fs-1" 
+            style={{width:"100px",height:"80px", marginRight:"10px"}}>
+                {number1}
+            </p>
+        </div>
     )
 };
 export default Card;
--- src/js/component/contador.jsx
+++ src/js/component/contador.jsx
@@ -3,7 +3,7 @@ import Card from "./card";
 
 const Contador =({number1, number2,number3,number4, number5, number6, number7}) => {
     return (
-        <div className="row bg-black">
+        <div className="row bg-black text-white">
             <Card number1={number1} number2={number2} number3={number3} number4={number4} number5={number5} number6={number6} number7={number7} />
            
         </div>
